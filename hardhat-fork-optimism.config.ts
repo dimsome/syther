3e3e02cbd5b18f9c0d5dd1f8af7be53e561f433e
Dimitri <69167058+dimsome@users.noreply.github.com>
2023-02-01 00:07:24 +0700

    change to earlier block

--- hardhat-fork-optimism.config.ts
+++ hardhat-fork-optimism.config.ts
@@ -1,18 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
-import hardhatConfig from "./hardhat.config"
-
-
+import hardhatConfig from "./hardhat.config";
 
 const config: HardhatUserConfig = {
-  ...hardhatConfig,
-  networks: {
-    hardhat: {
-      forking: {
-        url: "https://opt-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_OPTIMISM_API_KEY,
-        blockNumber: 71313791,
-      },
+    ...hardhatConfig,
+    networks: {
+        hardhat: {
+            forking: {
+                url: "https://opt-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_OPTIMISM_API_KEY,
+                blockNumber: 52488992,
+            },
+        },
     },
-  },
 };
 
 export default config;
